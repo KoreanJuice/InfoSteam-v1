3e9c513c4ae2fd831d5faec554de2f624153f0db
Marcos <monorata2@hotmail.com>
2020-12-08 04:20:08 +0100

    Added side nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'InfoSteam';
+export class AppComponent implements OnInit {
+
+  public title = 'InfoSteam';
+  public over = 'over';
+  public loading: boolean;
+
+  constructor(
+  ) { }
+
+  ngOnInit(): void {
+    this.loading = false;
+  }
+
 }
