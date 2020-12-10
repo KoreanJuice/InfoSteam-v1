969f1947f94e52bc1a0d275a1479b64a18c25e91
Marcos <monorata2@hotmail.com>
2020-12-10 01:26:57 +0100

    New components and routing
    * Detail components for game and user components
    * Better routing
    * Component for 404 page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { UserComponent } from './pages/user/user.component';
 import { GameComponent } from './pages/game/game.component';
+import { GameDetailComponent } from './pages/game-detail/game-detail.component';
+import { UserDetailComponent } from './pages/user-detail/user-detail.component';
 import { PriceTrackerComponent } from './pages/price-tracker/price-tracker.component';
 import { HelpComponent } from './pages/help/help.component';
 import { LegalComponent } from './pages/legal/legal.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -15,12 +18,24 @@ const routes: Routes = [
     component: HomeComponent,
   },
   {
-    path: 'user/:id',
+    path: 'user',
     component: UserComponent,
+    children: [
+      {
+        path: ':id',
+        component: UserDetailComponent,
+      },
+    ],
   },
   {
-    path: 'game/:id',
+    path: 'game',
     component: GameComponent,
+    children: [
+      {
+        path: ':id',
+        component: GameDetailComponent,
+      },
+    ],
   },
   {
     path: 'price-tracker',
@@ -33,6 +48,10 @@ const routes: Routes = [
   {
     path: 'legal',
     component: LegalComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 // Angular
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 // Modules
 import { AppRoutingModule } from './app-routing.module';
 import { material } from './shared/material/material';
@@ -9,9 +10,12 @@ import { material } from './shared/material/material';
 import { HomeComponent } from './pages/home/home.component';
 import { GameComponent } from './pages/game/game.component';
 import { UserComponent } from './pages/user/user.component';
+import { GameDetailComponent } from './pages/game-detail/game-detail.component';
+import { UserDetailComponent } from './pages/user-detail/user-detail.component';
 import { PriceTrackerComponent } from './pages/price-tracker/price-tracker.component';
 import { LegalComponent } from './pages/legal/legal.component';
 import { HelpComponent } from './pages/help/help.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 // Components
 import { AppComponent } from './app.component';
 import { SearchComponent } from './components/search/search.component';
@@ -37,11 +41,15 @@ import { ParseSearchPipe } from './shared/pipes/parse-search.pipe';
     GameDataComponent,
     UserDataComponent,
     ParseSearchPipe,
+    GameDetailComponent,
+    UserDetailComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     ...material,
   ],
   providers: [],
