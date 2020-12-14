a5d175226c83f0242f12d3ed5fbe08668323baeb
Marcos <monorata2@hotmail.com>
2020-12-14 02:35:16 +0100

    key file added

---
+++ src/sample.key.ts
@@ -0,0 +1,8 @@
+// Remove sample. from file name (✿◡‿◡)
+export const key = 'YOUR-STEAM-API-KEY';
+
+/* User Monorata2
+  steamid = 76561198094709860
+  customURL = http://steamcommunity.com/id/monorata2
+  profile = http://steamcommunity.com/profiles/76561198094709860
+*/
